c5b038ab6c1632f0bdc3c113a7b80170611bfd66
Sippius <90542599+Sippius@users.noreply.github.com>
2022-11-27 13:36:10 -0700

    Update skillchains.lua

--- skillchains/skillchains.lua
+++ skillchains/skillchains.lua
@@ -26,7 +26,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ]]
 
-addon.author = 'Ivaar';
+addon.author = 'Ivaar, sippius(v4)';
 addon.name = 'SkillChains';
 addon.version = '1.20.08.19';
 
